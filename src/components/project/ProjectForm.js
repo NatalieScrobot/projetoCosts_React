9687d6e86f6cd43503d321b0c2e9ae6066cd77af
Natalie Scrobot <natalie.scrobot@jallcard.com.br>
2021-12-22 10:53:53 -0300

    Criando API e utilizando fetch para  as categorias de Criar projeto

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import {useEffect, useState} from 'react'
 import styles from './ProjectForm.module.css'
 import Select from '../form/Select'
 import Input from '../form/Input'
 import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm({btnText}) {
+
+    const[categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method:'GET',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then((resp) => resp.json())
+          .then((data) => {
+            setCategories(data)
+          }).catch((err)=> console.log("Ocorreu um erro"))
+        
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input 
@@ -21,6 +38,7 @@ function ProjectForm({btnText}) {
             <Select
                 name="category_id"
                 text="Selecione a categoria"
+                options={categories}
             />
             <div>
                 <SubmitButton text={btnText} />
